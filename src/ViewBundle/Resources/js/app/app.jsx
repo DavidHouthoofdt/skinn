141d7e5c093daa8df3d04e4b74d252b4c1c2182f
david houthoofdt <david.houthoofdt@telenet.be>
2017-11-25 11:16:08 +0100

    Added the react start script

---
+++ src/ViewBundle/Resources/js/app/app.jsx
@@ -0,0 +1,18 @@
+/* @flow */
+
+import React, {Component} from 'react';
+
+class App extends Component {
+
+  render() {
+    return (
+      <div>
+        Hello world!
+      </div>
+    );
+  }
+
+}
+
+export default App;
+
