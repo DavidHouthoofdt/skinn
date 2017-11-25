e81b0ed65d496d28682d9e043998dfe4940d4d91
david houthoofdt <david.houthoofdt@telenet.be>
2017-11-25 10:32:06 +0100

    added insert/delete & update functions for the json manager

--- src/DataBundle/JsonDataManager.php
+++ src/DataBundle/JsonDataManager.php
@@ -10,6 +10,8 @@ namespace DataBundle;
 
 /**
  * Description of JsonDataManager
+ * I work with doctrine (normaly)
+ * just to speed this up
  *
  * @author david
  */
@@ -26,6 +28,13 @@ class JsonDataManager {
      */
     protected $file;
     
+    /**
+     * The constructor
+     * 
+     * @param string $file the json file
+     * 
+     * @throws \Exception
+     */
     public function __construct(string $file) {
         $this->file = __DIR__ . '/Data/' . $file;
         //simple converter ... just raw data return ...
@@ -60,5 +69,59 @@ class JsonDataManager {
         //again data should be validated ... but for purpose of the example
         //this is skipped 
         return file_put_contents($this->file, json_encode($data));
+    }
+    
+    /**
+     * Update the data
+     * 
+     * @param int $id
+     * @param array $data
+     * 
+     * @throws Exception
+     * 
+     * @return boolen
+     */
+    public function updateData(int $id, array $data) 
+    {
+        if (!isset($this->data[$id])) {
+            throw new \Exception(sprintf('id %d not found', $id));
+        }
+        $this->data[$id] = $data;
+        return $this->storeData($data);
+    }
+    
+     /**
+     * insert the data
+     * 
+     * @param array $data id col will be ignored
+     * 
+     * @throws Exception
+     * 
+     * @return int (new id)
+     */
+    public function insertData(array &$data) 
+    {
+        $newId = array_size($this->data) + 1;
+        $data['id'] = $newId;
+        $this->data[$newId] = $data;
+        return $newId;
+    }
+    
+    /**
+     * Delete the data
+     * 
+     * @param int $id
+     * 
+     * @throws Exception
+     * 
+     * @return boolen
+     */
+    public function deleteData(int $id) 
+    {
+        if (!isset($this->data[$id])) {
+            throw new \Exception(sprintf('id %d not found', $id));
+        }
+        unset($this->data[$id]);
+        return $this->storeData($data);
     }
 }
