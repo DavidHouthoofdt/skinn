c8454561f221cf9edd0a9a87e8fe62e4de2a6d59
david houthoofdt <david.houthoofdt@telenet.be>
2017-11-27 18:29:08 +0100

    Added base form components

---
+++ src/ViewBundle/Resources/js/app/components/FormInput.jsx
@@ -0,0 +1,46 @@
+/* @flow */
+
+import React, {Component} from 'react';
+
+type FormInputFieldType = 'text' | 'input';
+
+export type FormInputFieldValue = string | number;
+
+export type FormInputField = {
+  type: FormInputFieldType,
+  defaultValue?: FormInputFieldValue,
+  id?: string,
+  options?: Array<string>,
+  label?: string,
+};
+
+class FormInput extends Component {
+  
+  props: FormInputField;
+  
+  static defaultProps = {
+    type: 'input',
+  };
+    
+  getValue(): FormInputFieldValue {
+    return 'value' in this.refs.input
+      ? this.refs.input.value
+      : this.refs.input.getValue();
+  }
+
+  render() {
+    const common: Object = {
+      id: this.props.id,
+      ref: 'input',
+      defaultValue: this.props.defaultValue,
+    };
+    switch (this.props.type) {
+      case 'text':
+        return <textarea {...common} />;
+      default:
+        return <input {...common} type="text" />;
+    }
+  }
+}
+
+export default FormInput
