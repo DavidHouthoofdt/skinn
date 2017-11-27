3c85fd6b13b56e50743edc42b84046b4ef5f3e69
david houthoofdt <david.houthoofdt@telenet.be>
2017-11-27 18:23:22 +0100

    Added the dailog & button components

---
+++ src/ViewBundle/Resources/js/app/components/Button.jsx
@@ -0,0 +1,15 @@
+/* @flow */
+
+import React from 'react';
+
+
+type Props = {
+  href: ?string,
+};
+
+const Button = (props: Props) => 
+  props.href
+    ? <a {...props} className={props.className} />
+    : <button {...props}  className={props.className}  />
+
+export default Button
