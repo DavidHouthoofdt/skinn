12e321f1dc79cc49020bed4fa15073ec3ccfa5d4
David Houthoofdt <dhouthoofdt@buromac.be>
2017-11-28 14:26:54 +0100

    added the post actions for files

--- src/ApiBundle/Controller/FileController.php
+++ src/ApiBundle/Controller/FileController.php
@@ -59,13 +59,26 @@ class FileController extends AbstractController
      * Update the file
      *
      * @param int $id the file id
-     * 
+     *
      * @return type
      */
     public function putAction($id)
     {
         $req = new \ApiBundle\Components\Request\File\Put($this->getRequest());
         $res = new \ApiBundle\Components\Response\File\Put($req, $this->dataManager);
+        return $res->getResponse($this);
+    }
+
+    /**
+     * create a new file
+     *
+     *
+     * @return type
+     */
+    public function postAction()
+    {
+        $req = new \ApiBundle\Components\Request\File\Post($this->getRequest());
+        $res = new \ApiBundle\Components\Response\File\Post($req, $this->dataManager);
         return $res->getResponse($this);
     }
 }
